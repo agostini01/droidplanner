2b21a1340f7eba927824af8a854c3d6bf41410f7
Nicolas Agostini <n.b.agostini@gmail.com>
2014-08-12 18:48:07 -0300

    Prepare Desktop UI for multi drones

--- Desktop/src/org/droidplanner/desktop/ui/Menu.java
+++ Desktop/src/org/droidplanner/desktop/ui/Menu.java
@@ -21,8 +21,6 @@ public class Menu extends JMenuBar implements ActionListener {
 	
 	private Drone drone;
 
-	
-
 	public Menu(Drone drone) {
 		super();
 		this.drone = drone;
@@ -39,6 +37,30 @@ public class Menu extends JMenuBar implements ActionListener {
 		droneMenu.add(loadParameters);
 		droneMenu.add(openSettings);
 		add(droneMenu);
+		
+		JMenu droneInstancesMenu = new JMenu("Drone Instance");
+		JMenuItem droneSubMenu0 = new JMenuItem("Drone 0");
+		JMenuItem droneSubMenu1 = new JMenuItem("Drone 1");
+		JMenuItem droneSubMenu2 = new JMenuItem("Drone 2");
+		JMenuItem droneSubMenu3 = new JMenuItem("Drone 3");
+		JMenuItem droneSubMenu4 = new JMenuItem("Drone 4");
+		JMenuItem droneSubMenu5 = new JMenuItem("Drone 5");
+		JMenuItem droneSubMenu6 = new JMenuItem("Drone 6");
+		JMenuItem droneSubMenu7 = new JMenuItem("Drone 7");
+		JMenuItem droneSubMenu8 = new JMenuItem("Drone 8");
+		JMenuItem droneSubMenu9 = new JMenuItem("Drone 9");
+		
+		droneInstancesMenu.add(droneSubMenu0);
+		droneInstancesMenu.add(droneSubMenu1);
+		droneInstancesMenu.add(droneSubMenu2);
+		droneInstancesMenu.add(droneSubMenu3);
+		droneInstancesMenu.add(droneSubMenu4);
+		droneInstancesMenu.add(droneSubMenu5);
+		droneInstancesMenu.add(droneSubMenu6);
+		droneInstancesMenu.add(droneSubMenu7);
+		droneInstancesMenu.add(droneSubMenu8);
+		droneInstancesMenu.add(droneSubMenu9);
+		add(droneInstancesMenu);
 	}
 
 	@Override
